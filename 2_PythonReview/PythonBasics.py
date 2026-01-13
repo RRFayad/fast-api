ba4529cd71328cc3fdcb47a31676772a817afd9d
RRFayad <renan.r.fayad@gmail.com>
2026-01-12 21:11:38 -0300

    M02(Python Basics): Lists

--- 2_PythonReview/PythonBasics.py
+++ 2_PythonReview/PythonBasics.py
@@ -18,3 +18,34 @@ print(f"Remaining money after purchase: {money - total_cost}")
 
 sentence = "I had {} dollars, I bought an item for {} dollars (including tax of {}), and now I have {} dollars left."
 print(sentence.format(money, cost, cost * tax, money - total_cost))
+
+"""
+# Taking user input
+first_name = input("Enter your first name: ")
+print(f"Hello, {first_name}!")
+
+days = int(input("How many days until your birthday?"))
+print(f"Your birthday is in {round(days/7)} weeks!")
+"""
+
+
+# Lists in Python
+my_list = [80, 96, 72, 100, 8]
+print(my_list[-1])  # Accessing the last element of the list
+my_list[0:2]
+print(my_list)  # Accessing the last element of the list
+print(my_list[0:2])  # Accessing the last element of the list
+test = my_list[0:2]
+print(test)
+
+my_list.append(55)  # Adding an element to the end of the list
+print(my_list)
+
+my_list.remove(72)  # Removing an element from the list (looks for the value)
+print(my_list)
+
+my_list.sort()  # Sorting the list in ascending order
+print(my_list)
+
+my_list.reverse()  # Reversing the order of the list
+print(my_list)
--- Notes.md
+++ Notes.md
@@ -12,4 +12,20 @@
 
 ## Python Refresher
 
+- Linting: autopep8 extension
+
+- Remember we use snake_case for naming
+
 - Variables, comments, string formatting
+  `- print(f"Remaining money after purchase: {money}")`
+
+#### Lists
+
+- Same as arrays in JS, some obs and methods:
+  - `list_name[-1]` gets last item (or [-X] will get last x item)
+  - `list_name[0:2]` gets the slice from 0 up to 2 NOT included (and it does not mutate the list)
+  - `list_name.append()`
+  - `list_name.remove()`
+  - `list_name.pop()`
+  - `list_name.reverse()`
+  - `list_name.sort()`
