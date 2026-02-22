764d63882759e43025419fa987496889926367e5
RRFayad <renan.r.fayad@gmail.com>
2026-02-21 22:23:05 -0300

    M05(Proj 1): 70. Path parameters overview

--- __5_Proj1_Bookstore.md
+++ __5_Proj1_Bookstore.md
@@ -3,4 +3,12 @@
 - CRUD Operations & Http Requests
   - Swagger UI is what gives us the URLs?
 
+#### Run Server
+
+source .venv/bin/activate
+python -m uvicorn books:app --reload
+url: http://127.0.0.1:8000/...
+
+\*If we run `url/docs` if shows a Swagger UI
+
 ### Proj
--- __5_Project_1/books.py
+++ __5_Project_1/books.py
@@ -2,7 +2,34 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+BOOKS = [
+    {"id": 1, "title": "The Great Gatsby", "author": "F. Scott Fitzgerald"},
+    {"id": 2, "title": "To Kill a Mockingbird", "author": "Harper Lee"},
+    {"id": 3, "title": "1984", "author": "George Orwell"},
+    {"id": 4, "title": "Pride and Prejudice", "author": "Jane Austen"},
+    {"id": 5, "title": "The Catcher in the Rye", "author": "J.D. Salinger"},
+]
+
 
 @app.get("/books")
-def first_api():  # We dont need to explicit the async, fastapi will handle it for us (but we could)
-    return {"message": "Hello, World!"}
+def read_all_books():  # We dont need to explicit the async, fastapi will handle it for us (but we could)
+    return BOOKS
+
+
+@app.get("/books/search")
+# We can use query parameters to search for books by title or author
+# This static route should comes before the dynamic route, otherwise it will be treated as a dynamic route and we will never reach the search endpoint
+def search_books(query: str):
+    results = []
+    for book in BOOKS:
+        if query.lower() in book["title"].lower() or query.lower() in book["author"].lower():
+            results.append(book)
+    return results
+
+
+@app.get("/books/{book_id}")
+def get_book(book_id: int):
+    for book in BOOKS:
+        if book["id"] == book_id:
+            return book
+    return {"error": "Book not found"}
